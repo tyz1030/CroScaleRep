b753317c7ec1bcc640c9503a1fd7e3005141e39f
tyz <tyz@umich.edu>
2021-09-10 11:00:38 -0400

    api key

--- scripts/sample/sample_kempten.sh
+++ scripts/sample/sample_kempten.sh
@@ -1,13 +1,13 @@
 python3 $HOME/CroScaleRep/sample.py \
 --data $HOME/geo_loc_data/kempten/raw_telescope \
 --data_type .tif \
---output $HOME/geo_loc_data_gen/kempten1000 \
+--output $HOME/geo_loc_data_gen/kempten200 \
 --microscope_src google_map_static_api_and_tile \
 --telescope_src tiff \
---num_teles 1 \
+--num_teles 200 \
 --num_micros 32 \
 --sample_mode random \
---gmap_api_key AIzaSyDCYqIfRg-kLDEPDKFckzGIwn0nnP-__Sc \
---url_secret zgdrECVG2zvvHU8W5fz4RJKpUSg= \
+--gmap_api_key REPLACEWITHYOURKEY \
+--url_secret  REPLACEWITHYOURURLSECRET \
 --flip_axis 0 \
 --scale_down 1
